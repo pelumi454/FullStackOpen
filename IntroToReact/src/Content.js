133552d0c9c4e0b7b75d5e44392d51f8cf3b4549
pelumi454 <amoochristianah454@gmail.com>
2022-02-03 21:53:47 -0800

    remove exercise prop in content.js

--- IntroToReact/src/Content.js
+++ IntroToReact/src/Content.js
@@ -5,15 +5,12 @@ function Content({ part1, exercises1, part2, exercises2, part3, exercises3}) {
     <div>
       <div>
         <h5>{part1}</h5>
-        <p>{exercises1}</p>
       </div>
       <div>
         <h5>{part2}</h5>
-        <p>{exercises2}</p>
       </div>
       <div>
         <h5>{part3}</h5>
-        <p>{exercises3}</p>
       </div>
     </div>
   );
